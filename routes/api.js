b8fb9da929a47052019487b2491c40141c70f599
aldngrha <aldi.nugrahatk@gmail.com>
2022-10-12 23:10:20 +0700

    add routes booking api

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const LandingPageController = require("../controllers/API/LandingPageController");
 const DetailPageController = require("../controllers/API/DetailPageController");
-
-// const { uploadSingle } = require("../middlewares/multer");
+const BookingPageController = require("../controllers/API/BookingPageController");
+const { uploadSingle } = require("../middlewares/multer");
 
 router.get("/landing-page", LandingPageController.landingPage);
 router.get("/detail-page/:id", DetailPageController.detailPage);
+router.post("/booking-page", uploadSingle, BookingPageController.bookingPage);
 
 module.exports = router;

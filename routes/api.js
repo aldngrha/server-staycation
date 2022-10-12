bc2d10d20140219c823284f2feaa3eb66c53e47d
aldngrha <aldi.nugrahatk@gmail.com>
2022-10-12 19:59:23 +0700

    create routes api detail page

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
 const LandingPageController = require("../controllers/API/LandingPageController");
+const DetailPageController = require("../controllers/API/DetailPageController");
+
 // const { uploadSingle } = require("../middlewares/multer");
 
 router.get("/landing-page", LandingPageController.landingPage);
+router.get("/detail-page/:id", DetailPageController.detailPage);
 
 module.exports = router;

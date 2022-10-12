1f49adb4780479b0873876b120f718e08af1c493
aldngrha <aldi.nugrahatk@gmail.com>
2022-10-12 12:12:30 +0700

    read function to view booking

--- controllers/BookingController.js
+++ controllers/BookingController.js
@@ -1,9 +1,20 @@
-const Booking = require("../models/Item");
+const Moment = require("moment");
+const Booking = require("../models/Booking");
+const Member = require("../models/Member");
+const Bank = require("../models/Bank");
 
 module.exports = {
-  viewBooking: (req, res) => {
-    res.render("admin/booking/view_booking", {
-      title: "Staycation | Booking",
-    });
+  viewBooking: async (req, res) => {
+    try {
+      const booking = await Booking.find()
+        .populate("memberId")
+        .populate("bankId");
+      res.render("admin/booking/view_booking", {
+        moment: Moment,
+        title: "Staycation | Booking",
+        user: req.session.user,
+        booking,
+      });
+    } catch (error) {}
   },
 };

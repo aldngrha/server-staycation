392b3daeef03ae0be7f33add9b52ed965e3e91bf
aldngrha <aldi.nugrahatk@gmail.com>
2022-10-12 17:48:26 +0700

    view dashboard

--- controllers/DashboardController.js
+++ controllers/DashboardController.js
@@ -1,7 +1,22 @@
+const Member = require("../models/Member");
+const Booking = require("../models/Booking");
+const Item = require("../models/Item");
+
 module.exports = {
-  viewDashboard: (req, res) => {
-    res.render("admin/dashboard/view_dashboard", {
-      title: "Staycation | Dashboard",
-    });
+  viewDashboard: async (req, res) => {
+    try {
+      const member = await Member.find();
+      const booking = await Booking.find();
+      const item = await Item.find();
+      res.render("admin/dashboard/view_dashboard", {
+        title: "Staycation | Dashboard",
+        user: req.session.user,
+        member,
+        booking,
+        item,
+      });
+    } catch (error) {
+      res.redirect("/admin/dashboard");
+    }
   },
 };

cebb64174c9694b8ffdc29422124bd38f45dd630
aldngrha <aldi.nugrahatk@gmail.com>
2022-10-12 12:45:39 +0700

    adding function show detail booking

--- controllers/BookingController.js
+++ controllers/BookingController.js
@@ -15,6 +15,24 @@ module.exports = {
         user: req.session.user,
         booking,
       });
+    } catch (error) {
+      res.redirect("admin/booking");
+    }
+  },
+
+  showDetailBooking: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const booking = await Booking.findOne({ _id: id })
+        .populate("memberId")
+        .populate("bankId");
+      console.log(booking);
+      res.render("admin/booking/show_detail_booking", {
+        moment: Moment,
+        title: "Staycation | Detail Booking",
+        user: req.session.user,
+        booking,
+      });
     } catch (error) {}
   },
 };
